fd407014d25def7d30c78eaebd1fbf0f1339636b
codewithsaksham <63150776+codewithsaksham@users.noreply.github.com>
2020-04-13 02:06:30 +0530

    intial commit

---
+++ hello.py
@@ -0,0 +1,18 @@
+from flask  import Flask,render_template,redirect,url_for
+app=Flask(__name__)
+
+posts=[
+    {'author'}
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("xyz.html")
+
+@app.route("/about")
+def about():
+    return render_template('xyz.html')
+
+if __name__== "__main__" :
+    app.run(debug=True)
